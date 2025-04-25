25f07e4e24f94bd0b12c0469c5ba5c489edf29b9
Rohan12rt <rohan@iamtechie.com>
2025-04-25 10:04:42 +0530

    new file changes

--- src/Components/TodoWrappers.jsx
+++ src/Components/TodoWrappers.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
 import "../Css/TodoWrapper.css"
+import { v4 as uvidv4 } from 'uuid';
 
 function TodoWrappers() {
+  const [ todos,setTodos] = useState([]);
+  // add todo
+  const addTodo =(data)=>{
+    setTodos([
+      ...todos,{id:uvidv4(),task:data,completed:false,isEditing:false}
+    ])
+  }
+
+  console.log("todos",todos)
   return (
     <div> 
       <div className="todo-wrapper">
       <h1>TodoWrappers</h1>
-      <input type="text" placeholder="Add a task." className="todo-input" />
+      <TodoForm addTodo={addTodo} />
     </div>
     </div>
   ) 
